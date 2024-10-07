f2efdf252e7aba57731d1582b12663e01687be06
justin lovell <justinlvell18@gmail.com>
2024-10-07 10:59:23 +0700

    creating techData

---
+++ src/components/TechIcon.jsx
@@ -0,0 +1,7 @@
+
+
+export default function TechIcon() {
+  return (
+    <div>TechIcon</div>
+  )
+}
---
+++ src/data/TechData.js
@@ -0,0 +1,9 @@
+const TechData = [
+  { icon: faHtml5, color: "darkorange", isFontAwesome: true },
+  { icon: faCss3, color: "blue", isFontAwesome: true },
+  { icon: faJs, color: "yellow", isFontAwesome: true },
+  { icon: faReact, color: "#61dbfb", isFontAwesome: true },
+  { icon: SiTailwindcss, color: "#06B6D4", isFontAwesome: false },
+  { icon: faNode, color: "white", isFontAwesome: true },
+  { icon: SiExpress, color: "white", isFontAwesome: false },
+];
